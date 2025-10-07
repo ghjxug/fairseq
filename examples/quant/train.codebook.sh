066c9b3f60637021607246d0989bc33abed068ff
ghjxug <ghjxug@gmail.com>
2025-10-07 22:45:21 +0300

    fixed train script

--- examples/quant/train.codebook.sh
+++ examples/quant/train.codebook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BASEDIR=/path/to/working/dir
-path_2_data=/path/to/binarized/data
-lang_list=/path/to/langlist
+BASEDIR=./
+path_2_data=./bin_data
+lang_list=./langlist.txt
 
 nslices=16
 ncodes=10000
@@ -37,7 +37,7 @@ echo $valid_lang_pairs
 mkdir $FAIRSEQ_MODEL -p
 
 # Effective batch size is 2048 * 4 (GPUs) * 2 (update freq)
-CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train $path_2_data \
+CUDA_VISIBLE_DEVICES=0 fairseq-train $path_2_data \
   --user-dir examples/quant/codebook_transformer_src \
   --encoder-normalize-before --decoder-normalize-before \
   --share-all-embeddings \
@@ -49,23 +49,22 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 fairseq-train $path_2_data \
   --task translation_multi_simple_epoch \
   --sampling-method "temperature" \
   --sampling-temperature 5.0 \
-  --max-tokens 2048 \
+  --max-tokens 512 \
   --encoder-langtok "src" \
   --decoder-langtok \
   --lang-dict "$lang_list" \
   --lang-pairs "$lang_pairs" \
   --valid-lang-pairs "$valid_lang_pairs" \
-  --criterion encoder_similarity_label_smoothed_cross_entropy --label-smoothing 0.1 \
+  --criterion codebook_label_smoothed_cross_entropy --label-smoothing 0.1 \
   --optimizer adam --adam-eps 1e-06 --adam-betas '(0.9, 0.98)' \
   --lr-scheduler inverse_sqrt --lr 1e-04 --warmup-updates 2500 \
   --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
-  --update-freq 2 \
+  --update-freq 4 \
   --save-interval 1 --save-interval-updates 2000 --keep-interval-updates 10 --no-epoch-checkpoints \
   --keep-best-checkpoints 5 \
   --codebook-loss-weight 1.0 --commitment-loss-weight 1.001 \
   --codebook-slices $nslices --codebook-size $ncodes \
   --alternate-cont-chance $contchance \
   --seed 222 --log-format simple --log-interval 1000 --fp16 \
-  --finetune-from-model $BASEDIR/model/mm100_small.trimmed/model.pt \
   --max-update 500000 \
   --save-dir $FAIRSEQ_MODEL 2>&1 | tee -a $FAIRSEQ_MODEL/train.log
